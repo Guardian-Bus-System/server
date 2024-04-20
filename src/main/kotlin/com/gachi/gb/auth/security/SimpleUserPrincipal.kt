42b1206bfc500589a4043cd152874b2cbc85597d
wxodi0 <teayang06@gmail.com>
2024-04-20 12:08:19 +0900

    feat: JwtAuthProvider에서 UserId, authorities를 반한하기 위한 생성자

---
+++ src/main/kotlin/com/gachi/gb/auth/security/SimpleUserPrincipal.kt
@@ -0,0 +1,39 @@
+package com.gachi.gb.auth.security
+
+import org.springframework.security.core.GrantedAuthority
+import org.springframework.security.core.userdetails.UserDetails
+import java.beans.ConstructorProperties
+import java.util.UUID
+
+class SimpleUserPrincipal @ConstructorProperties("id", "authorities") constructor(
+  val id: UUID,
+  private val authorities: Collection<GrantedAuthority>
+): UserDetails {
+  override fun getAuthorities(): Collection<GrantedAuthority> {
+    return authorities
+  }
+
+  override fun getPassword(): String {
+    return ""
+  }
+
+  override fun getUsername(): String {
+    return id.toString()
+  }
+
+  override fun isAccountNonExpired(): Boolean {
+    return true
+  }
+
+  override fun isAccountNonLocked(): Boolean {
+    return true
+  }
+
+  override fun isCredentialsNonExpired(): Boolean {
+    return true
+  }
+
+  override fun isEnabled(): Boolean {
+    return true
+  }
+}
