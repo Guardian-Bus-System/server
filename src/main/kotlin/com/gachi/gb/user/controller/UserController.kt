66efe3824872a2f419680261875847cf6f37c0cf
wxodi0 <teayang06@gmail.com>
2024-04-20 12:12:27 +0900

    feat: User Contrller 추가

--- src/main/kotlin/com/gachi/gb/user/controller/UserController.kt
+++ src/main/kotlin/com/gachi/gb/user/controller/UserController.kt
@@ -1,21 +1,24 @@
 package com.gachi.gb.user.controller
 
+import com.gachi.gb.common.annotation.AuthUserId
 import com.gachi.gb.user.domain.User
 import com.gachi.gb.user.service.UserService
+import org.springframework.security.core.Authentication
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.PathVariable
+import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
+import java.util.UUID
 
 @RestController
 @RequestMapping("/users")
 class UserController (
   private val userService: UserService
 ) {
-  @GetMapping("/user")
-  fun getUser(userId: String): User {
-    return userService.getUser(userId)
+  @GetMapping("/me")
+  fun getByUserId(@AuthUserId id: UUID): User {
+    return userService.getUserById(id)
   }
-
 }
