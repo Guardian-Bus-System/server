aa09fe273bede5f501ceee20a5a78c8878785a14
wxodi0 <teayang06@gmail.com>
2024-04-20 11:56:50 +0900

    feat: SecurityConfig 추가

--- src/main/kotlin/com/gachi/gb/auth/config/SecurityConfig.kt
+++ src/main/kotlin/com/gachi/gb/auth/config/SecurityConfig.kt
@@ -1,16 +1,20 @@
 package com.gachi.gb.auth.config
 
+import com.gachi.gb.auth.security.filter.JwtAuthenticationFilter
 import org.springframework.context.EnvironmentAware
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 import org.springframework.core.env.Environment
 import org.springframework.http.HttpMethod
 import org.springframework.security.authentication.AuthenticationManager
+import org.springframework.security.config.annotation.SecurityConfigurerAdapter
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration
 import org.springframework.security.config.annotation.web.builders.HttpSecurity
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder
+import org.springframework.security.web.DefaultSecurityFilterChain
 import org.springframework.security.web.SecurityFilterChain
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter
 import org.springframework.web.cors.CorsConfiguration
 import org.springframework.web.cors.CorsConfigurationSource
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource
@@ -19,7 +23,7 @@ import java.util.*
 @Configuration
 @EnableWebSecurity
 class SecurityConfig (
-): EnvironmentAware {
+): EnvironmentAware, SecurityConfigurerAdapter<DefaultSecurityFilterChain, HttpSecurity>() {
   lateinit var env: Environment
 
   override fun setEnvironment(environment: Environment) {
@@ -27,7 +31,10 @@ class SecurityConfig (
   }
 
   @Bean
-  fun filterChain(http: HttpSecurity): SecurityFilterChain {
+  fun filterChain(
+    authenticationConfiguration: AuthenticationConfiguration,
+    http: HttpSecurity
+  ): SecurityFilterChain {
     //동일한 출처의 프레임 허용
     //Clickjacking 공격을 방지하기 위해.
     http.headers {
@@ -38,6 +45,8 @@ class SecurityConfig (
     http.cors {
       it.configurationSource(corsConfigurationSource())
     }
+    http.addFilterBefore(JwtAuthenticationFilter(authenticationManager(authenticationConfiguration)), UsernamePasswordAuthenticationFilter::class.java)
+
     http.authorizeHttpRequests {
       //정적 소스 허용
       it.requestMatchers(
@@ -107,6 +116,11 @@ class SecurityConfig (
     val source = UrlBasedCorsConfigurationSource()
     source.registerCorsConfiguration("/**", configuration)
     return source
+  }
+
+  @Bean
+  fun JwtAuthenticationFilter(authenticationManager: AuthenticationManager): JwtAuthenticationFilter? {
+    return JwtAuthenticationFilter(authenticationManager)
   }
 
   @Bean
