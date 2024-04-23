957a18f44a985eb14c92ce51ab7dcb7d19fb6ccb
wxodi0 <teayang06@gmail.com>
2024-04-23 19:47:04 +0900

    feat: refresh 인증용 Authentication 추가

---
+++ src/main/kotlin/com/gachi/gb/auth/security/authentication/RefreshAuthentication.kt
@@ -0,0 +1,14 @@
+package com.gachi.gb.auth.security.authentication
+
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken
+import org.springframework.security.core.GrantedAuthority
+
+class RefreshAuthentication: UsernamePasswordAuthenticationToken {
+  constructor(principal: Any?, credentials: Any?) : super(
+    principal, credentials
+  )
+
+  constructor(principal: Any?, credentials: Any?, authorities: Collection<GrantedAuthority>): super(
+    principal, credentials, authorities
+  )
+}
