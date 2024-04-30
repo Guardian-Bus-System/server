564cd079312d6e35e6381c0b78a95a1e7e63198d
wxodi0 <teayang06@gmail.com>
2024-04-30 22:38:39 +0900

    feat: SecurityConfig PathPattern 오류 수정

--- src/main/kotlin/com/gachi/gb/auth/config/SecurityConfig.kt
+++ src/main/kotlin/com/gachi/gb/auth/config/SecurityConfig.kt
@@ -1,5 +1,6 @@
 package com.gachi.gb.auth.config
 
+import com.gachi.gb.auth.security.JwtUtils
 import com.gachi.gb.auth.security.filter.JwtAuthenticationFilter
 import org.springframework.context.EnvironmentAware
 import org.springframework.context.annotation.Bean
@@ -11,6 +12,7 @@ import org.springframework.security.authentication.AuthenticationProvider
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration
 import org.springframework.security.config.annotation.web.builders.HttpSecurity
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity
+import org.springframework.security.config.http.SessionCreationPolicy
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder
 import org.springframework.security.crypto.password.PasswordEncoder
 import org.springframework.security.web.SecurityFilterChain
@@ -34,19 +36,32 @@ class SecurityConfig (
   @Bean
   fun filterChain(
     authenticationConfiguration: AuthenticationConfiguration,
-    http: HttpSecurity
+    http: HttpSecurity,
+    jwtUtils: JwtUtils
   ): SecurityFilterChain {
     //동일한 출처의 프레임 허용
     //ClickJacking 공격을 방지하기 위해.
     http.headers {
       it.frameOptions{ frames ->
-        frames.sameOrigin()
+        frames.disable()
       }
     }
     http.cors {
       it.configurationSource(corsConfigurationSource())
     }
-    http.addFilterBefore(JwtAuthenticationFilter(authenticationManager(authenticationConfiguration)), UsernamePasswordAuthenticationFilter::class.java)
+
+    http.csrf {
+      it.disable()
+        .addFilterBefore(authenticationFilter(authenticationManager(authenticationConfiguration)), UsernamePasswordAuthenticationFilter::class.java)
+    }
+
+//    http.exceptionHandling {
+
+//    }
+
+    http.sessionManagement {
+      it.sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+    }
 
     http.authorizeHttpRequests {
       //정적 소스 허용
@@ -61,27 +76,27 @@ class SecurityConfig (
       ).permitAll()
 
       it.requestMatchers(
-        "/**", //TODO: 테스팅용으로 모든 request 허용
+        "/**",
         "/auth/**",
         "/user/**"
       ).permitAll()
 
-      it.requestMatchers(HttpMethod.OPTIONS, "/**")
+      it.requestMatchers(HttpMethod.OPTIONS, "/**/*")
         .permitAll()
-        .anyRequest().authenticated()
-    }
-    http.formLogin {
-      it.loginPage("/login")
-        .defaultSuccessUrl("/articles")
-    }
-    http.logout {
-      it.logoutSuccessUrl("/login")
-        .invalidateHttpSession(true)
-    }
-    http.csrf {
-      it.disable()
-    }
 
+      it.anyRequest()
+        .authenticated()
+    }
+//    http.formLogin {
+//      it.loginPage("/login")
+//        .defaultSuccessUrl("/articles")
+//    }
+//    http.logout {
+//      it.logoutSuccessUrl("/login")
+//        .invalidateHttpSession(true)
+//    }
+
+    //provider등록
     authProviders!!.forEach {
       http.authenticationProvider(it)
     }
@@ -93,13 +108,13 @@ class SecurityConfig (
     val configuration = CorsConfiguration()//요청이 허용되지 않은 새 인스턴스
     configuration.allowedOriginPatterns = listOf(
       // 추후 도메인 추가
-      "*"
+      "**"
     )
     if (Arrays.stream(env.activeProfiles)
         .anyMatch { profile: String -> "dev" == profile || "local" == profile || "linkprod" == profile || "staging" == profile }
     ) {
       configuration.allowedOriginPatterns = listOf(
-        "*"
+        "**"
       )
     }
     configuration.allowedMethods = listOf(
