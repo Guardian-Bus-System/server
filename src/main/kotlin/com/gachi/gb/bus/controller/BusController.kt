f58e6ae20f4d0854d8241b092feae7f697523d4f
wxodi0 <teayang06@gmail.com>
2024-05-07 23:30:44 +0900

    feat: bus 전체조회 및 일괄조회 controller 추가

---
+++ src/main/kotlin/com/gachi/gb/bus/controller/BusController.kt
@@ -0,0 +1,28 @@
+package com.gachi.gb.bus.controller
+
+import com.gachi.gb.bus.domain.Bus
+import com.gachi.gb.bus.service.BusService
+import com.gachi.gb.common.response.CommonResponse
+import com.gachi.gb.common.response.CommonResponse.Companion.ok
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.PathVariable
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RestController
+
+@RestController
+@RequestMapping("/bus")
+class BusController (
+  private val busService: BusService
+) {
+  //버스 일괄 조회
+  @GetMapping("/{busId}")
+  fun getBus(@PathVariable busId: Int): CommonResponse<Bus> {
+    return ok(busService.getBus(busId))
+  }
+
+  //버스 목록 조회
+  @GetMapping("/buses")
+  fun getBuses(): CommonResponse<List<Bus>> {
+    return ok(busService.getBuses())
+  }
+}
