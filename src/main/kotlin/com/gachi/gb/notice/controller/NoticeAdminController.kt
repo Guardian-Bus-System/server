a2e5f93a9fd58c9875b1ba893e6d2cc2a6d2977c
wxodi0 <teayang06@gmail.com>
2024-05-16 23:45:01 +0900

    feat: 관리자용 공지 Controller 추가

---
+++ src/main/kotlin/com/gachi/gb/notice/controller/NoticeAdminController.kt
@@ -0,0 +1,11 @@
+package com.gachi.gb.notice.controller
+
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RestController
+
+@RestController
+@RequestMapping("/admin/notice")
+class NoticeAdminController (
+
+) {
+}
