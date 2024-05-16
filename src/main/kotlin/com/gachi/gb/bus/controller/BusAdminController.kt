6149847a907a6b654b74c6b8ccf49695e130b922
wxodi0 <teayang06@gmail.com>
2024-05-16 23:43:40 +0900

    feat: Bus 기능에 Swagger 추가

--- src/main/kotlin/com/gachi/gb/bus/controller/BusAdminController.kt
+++ src/main/kotlin/com/gachi/gb/bus/controller/BusAdminController.kt
@@ -6,6 +6,8 @@ import com.gachi.gb.bus.dto.BusUpdateAdminDto
 import com.gachi.gb.bus.service.BusAdminService
 import com.gachi.gb.common.response.CommonResponse
 import com.gachi.gb.common.response.CommonResponse.Companion.ok
+import io.swagger.v3.oas.annotations.Operation
+import io.swagger.v3.oas.annotations.tags.Tag
 import org.springframework.web.bind.annotation.DeleteMapping
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.PathVariable
@@ -15,26 +17,31 @@ import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.ResponseBody
 import org.springframework.web.bind.annotation.RestController
 
+@Tag(name = "관리자 버스", description = "관리자용 버스 관리 API")
 @RestController
 @RequestMapping("/admin/buses")
 class BusAdminController (
   private val busAdminService: BusAdminService
 ) {
+  @Operation(summary = "버스 상세 조회", description = "버스 id를 통한 개별 조회 API")
   @GetMapping("/{busId}")
   fun getBus(@PathVariable busId: Int): CommonResponse<Bus> {
     return ok(busAdminService.getBus(busId))
   }
 
+  @Operation(summary = "버스 목록 조회", description = "등록된 버스 목록 조회 API")
   @GetMapping("/")
   fun getBuses(): CommonResponse<List<Bus>> {
     return ok(busAdminService.getBuses())
   }
 
+  @Operation(summary = "버스 추가", description = "버스 추가 API")
   @GetMapping("/bus/add")
   fun addBus(@RequestBody dto: BusAddAdminDto): CommonResponse<String> {
     return ok(busAdminService.addBus(dto))
   }
 
+  @Operation(summary = "버스 수정 API", description = "버스 정보를 수정할 수 있는 API")
   @PutMapping("/{busId}")
   fun updateBus(
     @PathVariable busId: Int,
@@ -43,6 +50,7 @@ class BusAdminController (
     return ok(busAdminService.updateBus(busId, dto))
   }
 
+  @Operation(summary = "버스 일괄 삭제 API", description = "버스를 개별 삭제 API")
   @DeleteMapping("/{busId}")
   fun deleteBus(@PathVariable busId: Int): CommonResponse<String> {
     return ok(busAdminService.deleteBus(busId))
--- src/main/kotlin/com/gachi/gb/bus/controller/BusController.kt
+++ src/main/kotlin/com/gachi/gb/bus/controller/BusController.kt
@@ -4,23 +4,29 @@ import com.gachi.gb.bus.domain.Bus
 import com.gachi.gb.bus.service.BusService
 import com.gachi.gb.common.response.CommonResponse
 import com.gachi.gb.common.response.CommonResponse.Companion.ok
+import io.swagger.v3.oas.annotations.Operation
+import io.swagger.v3.oas.annotations.tags.Tag
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.PathVariable
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
+import java.util.UUID
 
+@Tag(name = "사용자 버스", description = "사용자용 버스 조회 및 탑승 추가 API")
 @RestController
 @RequestMapping("/buses")
 class BusController (
   private val busService: BusService
 ) {
   //버스 일괄 조회
+  @Operation(summary = "버스 상세 조회", description = "버스 id를 통한 개별 조회 API")
   @GetMapping("/{busId}")
-  fun getBus(@PathVariable busId: Int): CommonResponse<Bus> {
+  fun getBus(@PathVariable busId: UUID): CommonResponse<Bus> {
     return ok(busService.getBus(busId))
   }
 
   //버스 목록 조회
+  @Operation(summary = "버스 목록 조회", description = "버스의 목록을 조회할 수 있음")
   @GetMapping("/")
   fun getBuses(): CommonResponse<List<Bus>> {
     return ok(busService.getBuses())
