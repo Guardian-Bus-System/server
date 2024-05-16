d9b0a2d2d9b6437284edbe1aac1fc0e98b8cf3fc
wxodi0 <teayang06@gmail.com>
2024-05-16 23:44:38 +0900

    feat: Swagger implementation 추가

--- build.gradle.kts
+++ build.gradle.kts
@@ -44,6 +44,11 @@ dependencies {
   testImplementation("org.springframework.boot:spring-boot-starter-test")
   testImplementation("org.springframework.security:spring-security-test")
 
+  //Swagger 적용
+//  implementation("io.springfox:springfox-boot-starter:3.0.0")
+//  implementation("io.springfox:springfox-swagger2:3.0.0")
+//  implementation("io.springfox:springfox-swagger-ui:3.0.0")
+  implementation("org.springdoc:springdoc-openapi-starter-webmvc-ui:2.0.2")
 }
 
 tasks.withType<KotlinCompile> {
