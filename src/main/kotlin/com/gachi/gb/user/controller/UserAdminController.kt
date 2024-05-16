9607393826299c314111ac1fbb6f4f28302689dd
wxodi0 <teayang06@gmail.com>
2024-05-16 23:42:09 +0900

    feat: User에 Swagger 추가

--- src/main/kotlin/com/gachi/gb/user/controller/UserAdminController.kt
+++ src/main/kotlin/com/gachi/gb/user/controller/UserAdminController.kt
@@ -5,6 +5,8 @@ import com.gachi.gb.common.response.CommonResponse.Companion.ok
 import com.gachi.gb.user.domain.User
 import com.gachi.gb.user.dto.UserAdminUpdateDto
 import com.gachi.gb.user.service.UserAdminService
+import io.swagger.v3.oas.annotations.Operation
+import io.swagger.v3.oas.annotations.tags.Tag
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.PutMapping
 import org.springframework.web.bind.annotation.RequestBody
@@ -12,16 +14,19 @@ import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.ResponseBody
 import org.springframework.web.bind.annotation.RestController
 
+@Tag(name = "관리자_사용자", description = "관리자용 사용자 관리 API")
 @RestController
 @RequestMapping("/admin/users")
 class UserAdminController (
   private val userAdminService: UserAdminService
 ) {
   @GetMapping("/")
+  @Operation(summary = "사용자 목록 조회", description = "모든 유저 조회")
   fun getUsers(): CommonResponse<List<User>> {
     return ok(userAdminService.getUsers())
   }
 
+  @Operation(summary = "사용자 수정", description = "유저 수정 API")
   @PutMapping("/user")
   fun updateUser(@RequestBody dto: UserAdminUpdateDto): CommonResponse<String> {
     return ok(userAdminService.updateUser(dto))
--- src/main/kotlin/com/gachi/gb/user/controller/UserController.kt
+++ src/main/kotlin/com/gachi/gb/user/controller/UserController.kt
@@ -4,7 +4,10 @@ import com.gachi.gb.common.annotation.AuthUserId
 import com.gachi.gb.common.response.CommonResponse
 import com.gachi.gb.common.response.CommonResponse.Companion.ok
 import com.gachi.gb.user.domain.User
+import com.gachi.gb.user.dto.UserUpdateDto
 import com.gachi.gb.user.service.UserService
+import io.swagger.v3.oas.annotations.Operation
+import io.swagger.v3.oas.annotations.tags.Tag
 import org.springframework.security.core.Authentication
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.PathVariable
@@ -14,11 +17,13 @@ import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
 import java.util.UUID
 
+@Tag(name = "일반 사용자", description = "일반 사용자 API")
 @RestController
 @RequestMapping("/users")
 class UserController (
   private val userService: UserService
 ) {
+  @Operation(summary = "본인 프로필 조회", description = "토큰을 통한 본인 프로필 조회")
   @GetMapping("/me")
   fun getByUserId(@AuthUserId userId: String): CommonResponse<User> {
     return ok(userService.getUserByLoginId(userId))
