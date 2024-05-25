3efc6c215c129b97455f7915a6a280c8132f0718
wxodi0 <teayang06@gmail.com>
2024-05-25 17:29:48 +0900

    feat: DTO에 따른 return 값 수정

--- src/main/kotlin/com/gachi/gb/notice/service/NoticeService.kt
+++ src/main/kotlin/com/gachi/gb/notice/service/NoticeService.kt
@@ -1,11 +1,13 @@
 package com.gachi.gb.notice.service
 
 import com.gachi.gb.notice.domain.Notice
+import com.gachi.gb.notice.dto.response.NoticeListResponseDto
+import com.gachi.gb.notice.dto.response.NoticeResponseDto
 import java.util.UUID
 
 interface NoticeService {
-  fun getNotice(id: UUID): Notice
+  fun getNotice(id: UUID): NoticeResponseDto
 
-  fun getNotices(): MutableList<Notice>
+  fun getNotices(): List<NoticeListResponseDto>
 
 }
