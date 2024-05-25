b5b09da516d4a3ebd3d4d71c2d7c95a51cd802b7
wxodi0 <teayang06@gmail.com>
2024-05-25 17:31:19 +0900

    feat: DTO에 따른 return 값 수정

--- src/main/kotlin/com/gachi/gb/notice/service/impl/NoticeServiceImpl.kt
+++ src/main/kotlin/com/gachi/gb/notice/service/impl/NoticeServiceImpl.kt
@@ -1,6 +1,8 @@
 package com.gachi.gb.notice.service.impl
 
 import com.gachi.gb.notice.domain.Notice
+import com.gachi.gb.notice.dto.response.NoticeListResponseDto
+import com.gachi.gb.notice.dto.response.NoticeResponseDto
 import com.gachi.gb.notice.repository.NoticeRepository
 import com.gachi.gb.notice.service.NoticeService
 import org.springframework.stereotype.Service
@@ -10,13 +12,30 @@ import java.util.*
 class NoticeServiceImpl(
   private val noticeRepository: NoticeRepository
 ): NoticeService {
-  override fun getNotice(id: UUID): Notice {
-    return noticeRepository.findById(id).orElseThrow {
-      IllegalArgumentException("해당 공지에 대한 주소가 잘못되었습니다.")
+  override fun getNotice(id: UUID): NoticeResponseDto {
+    val notice = noticeRepository.findById(id).orElseThrow {
+      IllegalArgumentException("공지사항이 존재하지 않습니다.")
     }
+    return NoticeResponseDto(
+      notice.id,
+      notice.title,
+      notice.content,
+      notice.tag,
+      notice.createAt,
+      notice.updateAt,
+      notice.uploadUser
+    )
   }
 
-  override fun getNotices(): MutableList<Notice> {
-    return noticeRepository.findAll()
+  override fun getNotices(): List<NoticeListResponseDto> {
+    return noticeRepository.findAll().map {
+      NoticeListResponseDto(
+        it.id,
+        it.title,
+        it.tag,
+        it.createAt,
+        it.updateAt
+      )
+    }
   }
 }
