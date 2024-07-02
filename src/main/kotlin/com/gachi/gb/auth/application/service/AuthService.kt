8d825e57efe1813bdd84130fb97a83d96ee207f9
wxodi0 <teayang06@gmail.com>
2024-07-02 17:42:57 +0900

    feat: role 수정 및 bus 관련 구조 수정

--- src/main/kotlin/com/gachi/gb/auth/application/service/AuthService.kt
+++ src/main/kotlin/com/gachi/gb/auth/application/service/AuthService.kt
@@ -4,9 +4,8 @@ import com.gachi.gb.auth.security.token.IdPasswordAuthentication
 import com.gachi.gb.auth.security.token.RefreshAuthentication
 import com.gachi.gb.auth.application.domain.JwtToken
 import com.gachi.gb.auth.application.domain.RefreshToken
-import com.gachi.gb.bus.domain.BusDetails
 import com.gachi.gb.bus.repository.BusRepository
-import com.gachi.gb.user.domain.Role
+import com.gachi.gb.user.domain.Roles
 import com.gachi.gb.user.domain.User
 import com.gachi.gb.user.repository.RoleRepository
 import com.gachi.gb.user.repository.UserRepository
@@ -24,47 +23,31 @@ class AuthService (
   private val passwordEncoder: PasswordEncoder,
   private val authenticationManager: AuthenticationManager,
   private val createTokenService: CreateTokenService,
-  private val busRepository: BusRepository
 ) {
   fun join(dto: UserJoinDto): JwtToken {
     if(userRepository.existsByLoginId(dto.loginId)) {
       throw IllegalArgumentException("중복되는 아이디입니다.")
     }
 
-    val role = roleRepository.findById("USER").orElseGet {
-      roleRepository.save(Role("USER", "사용자"))
+//    val role = roleRepository.findById("ADMIN").orElseGet {
+//      roleRepository.save(Roles("ADMIN", "관리자"))
+//    }
+
+    val userRole = roleRepository.findById("USER").orElseGet {
+      roleRepository.save(Roles("USER", "사용자"))
     }
-
-    val bus = busRepository.findByBusNumber(dto.boardingBus).orElseThrow {
-      IllegalArgumentException("해당되는 버스를 찾을 수 없습니다.")
-    }
-
-    //상세 호차 정보 반환
-    bus.lines = bus.lines.filter {
-      it.detailsLine == dto.detailsLine
-    } as MutableList<BusDetails>
-
-    //변경 버스 여부 -> 이떄 없으면 기존의 경로 반환
-    val changeBus = busRepository.findByBusNumber(dto.changeBoardingBus).orElse(bus)
-
-    changeBus.lines = changeBus.lines.filter {
-      it.detailsLine == dto.detailsLine
-    } as MutableList<BusDetails>
-
 
     val user = User(
       null,
       dto.loginId,
       passwordEncoder.encode(dto.pw),
       dto.name,
-      dto.call,
+      dto.phoneNumber,
       dto.gradeClass,
       dto.number,
-      dto.usingCk,
-      bus,
-      changeBus,
+      false,
       null,
-      mutableListOf(role)
+      mutableListOf(userRole)
     )
 
     userRepository.save(user)
@@ -89,13 +72,7 @@ class AuthService (
     val authentication = authenticationManager.authenticate(
       RefreshAuthentication(null, dto.refreshToken)
     )
-    val token = createToken(authentication)
-
-    //Access Token의 유효기간을 체크하고 RefreshToken을 통해 assccToken을 재발급 해야됨
-    return JwtToken(
-      "",
-      ""
-    )
+    return createToken(authentication)
   }
 
   fun createToken(authentication: Authentication): JwtToken {
