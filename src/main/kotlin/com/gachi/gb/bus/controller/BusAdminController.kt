72ad75f8263b9383b37ee6d03e989bacf438c40f
wxodi0 <teayang06@gmail.com>
2024-07-03 23:48:25 +0900

    feat: annotation 묶기

--- src/main/kotlin/com/gachi/gb/bus/controller/BusAdminController.kt
+++ src/main/kotlin/com/gachi/gb/bus/controller/BusAdminController.kt
@@ -7,14 +7,7 @@ import com.gachi.gb.common.response.CommonResponse
 import com.gachi.gb.common.response.CommonResponse.Companion.ok
 import io.swagger.v3.oas.annotations.Operation
 import io.swagger.v3.oas.annotations.tags.Tag
-import org.springframework.web.bind.annotation.DeleteMapping
-import org.springframework.web.bind.annotation.GetMapping
-import org.springframework.web.bind.annotation.PathVariable
-import org.springframework.web.bind.annotation.PutMapping
-import org.springframework.web.bind.annotation.RequestBody
-import org.springframework.web.bind.annotation.RequestMapping
-import org.springframework.web.bind.annotation.ResponseBody
-import org.springframework.web.bind.annotation.RestController
+import org.springframework.web.bind.annotation.*
 
 @Tag(name = "관리자 버스", description = "관리자용 버스 관리 API")
 @RestController
